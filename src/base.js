3991b0b81efc7a4e84e996e75d2899f7bf0b0b44
Ravenhiv <ravenhiv@gmail.com>
2017-02-23 18:00:27 +0300

    1-19 lessons done

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAQUNjtq4w7WoIahNff4rI4G_LYasPS7h8",
+  authDomain: "catch-of-the-day-zac-raven.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-zac-raven.firebaseio.com"
+});
+
+export default base;
